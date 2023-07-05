e3cdf666e32a6b9736f0ce19c94c5188494b003e
DaurDia <patiencedaur@gmail.com>
2023-07-05 11:54:17 +0200

    Refactor scripts

--- marytreat/scripts/publish_pdf_to_portals.py
+++ marytreat/scripts/publish_pdf_to_portals.py
@@ -1,8 +1,13 @@
 import _initialize
 from marytreat.core.tridionclient import Publication
 from _validator import get_guid_from_cli
+from msvcrt import getch
 
 guid = get_guid_from_cli('Enter publication GUID or copy the publication (p_***) string here: ')
 
 pub = Publication(id=guid)
 pub.publish_to_portals()
+
+print('Set Publish to Portals to Yes for {}. Press any key to quit'.format(guid))
+
+getch()
