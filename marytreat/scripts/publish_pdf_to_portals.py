ec2865302e264cd7e2d1b29ac330a83fa3cb9111
DaurDia <patiencedaur@gmail.com>
2023-07-17 09:16:52 +0200

    Show message only on error

--- marytreat/scripts/publish_pdf_to_portals.py
+++ marytreat/scripts/publish_pdf_to_portals.py
@@ -3,11 +3,11 @@ from marytreat.core.tridionclient import Publication
 from _validator import get_guid_from_cli
 from msvcrt import getch
 
-guid = get_guid_from_cli('Enter publication GUID or copy the publication (p_***) string here: ')
+try:
+    guid = get_guid_from_cli('Enter publication GUID or copy the publication (p_***) string here: ')
 
-pub = Publication(id=guid)
-pub.publish_to_portals()
-
-print('Set Publish to Portals to Yes for {}. Press any key to quit'.format(guid))
-
-getch()
+    pub = Publication(id=guid)
+    pub.publish_to_portals()
+except Exception as e:
+    print('Cannot publish {} to portals. Reason:\n{}'.format(guid, e))
+    getch()
